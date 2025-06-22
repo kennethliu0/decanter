1fb91d937ae18a15a7d17c6b57ed01fe416b9041
gearsoft66 <98987701+kennethliu0@users.noreply.github.com>
2025-06-22 15:02:40 -0500

    Set theme to dark mode with sky accent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Theme accentColor="red">
+        <Theme
+          appearance="dark"
+          accentColor="sky"
+        >
           {children}
           <ThemePanel />
         </Theme>

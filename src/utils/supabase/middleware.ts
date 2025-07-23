be69728b2fda4ba5fc0c838e81ac28fc7bff7fea
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2025-07-23 16:54:32 -0500

    Refactor middleware to use getClaims() in preparation for JWT signing keys

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -30,14 +30,13 @@ export async function updateSession(request: NextRequest) {
   );
 
   // Do not run code between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
+  // supabase.auth.getClaims(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
-  // IMPORTANT: DO NOT REMOVE auth.getUser()
+  // IMPORTANT: DO NOT REMOVE auth.getClaims()
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getClaims();
+  const user = data?.claims;
 
   if (
     !user
@@ -47,7 +46,7 @@ export async function updateSession(request: NextRequest) {
     && !request.nextUrl.pathname.startsWith("/auth")
     && request.nextUrl.pathname !== "/"
   ) {
-    // no user, potentially respond by redirecting the user to the login page
+    // no user, respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.redirect(url);

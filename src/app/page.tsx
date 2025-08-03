93def93a78571230656572aa66ad46a7edd16791
Jay <jaysahni70@gmail.com>
2025-08-03 14:52:08 -0500

    Standardized text markup to primary and renamed the hero_header to hero_banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
     <>
       <Header />
       <main className="w-full grow flex flex-col items-center">
-        <div className="flex xl:flex-row flex-col items-center justify-between w-full px-4 py-30">
-          <div className="relative xl:w-full order-first xl:order-last">
-            <img src='/hero_header.svg' alt = "logo" className="w-full"></img>
+        <div className="flex flex-col items-center justify-between w-full px-4 py-10 bg-[radial-gradient(circle_at_50%_50%,var(--muted),transparent_30%)]">
+          <div className="relative order-first ">
+            <img src='/hero_header.svg' alt = "logo" className="w-full"/>
           </div>
-          <div className="relative px-20 py-3 ">
-            <h1 className="text-6xl text-sky-300 font-bold  break-words p">Seamless Volunteering.</h1>
-            <p className="text-1xl py-2 break-words">An open source platform connecting Science Olympiad tournaments to volunteers nationwide.</p>
-          </div>
+        </div>
+        <div className="relative px-20 py-3">
+          <h1 className="text-center text-6xl text-primary font-bold break-words">Seamless Volunteering</h1>
+          <p className="text-center text-1xl py-2 break-words">An open source platform connecting Science Olympiad tournaments to volunteers nationwide.</p>
         </div>
         <div className="w-full flex gap-4 place-content-center-safe">
             <Link href="/signup">

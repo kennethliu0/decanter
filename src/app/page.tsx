5f9ab03fd01ec7e52c93f465b35a80f6bc454c75
Jay <jaysahni70@gmail.com>
2025-08-03 16:27:47 -0500

    Added background blur to logo and centered hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,23 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="w-full grow flex flex-col items-center">
-        <div className="flex flex-col items-center justify-between w-full px-4 py-10 bg-[radial-gradient(circle_at_50%_50%,var(--muted),transparent_30%)]">
-          <div className="relative order-first ">
-            <img src='/hero_header.svg' alt = "logo" className="w-full"/>
+      <main className="w-full grow flex flex-col items-center p-5">
+        <div className="relative flex flex-col items-center w-full py-12 bg-[radial-gradient(circle_at_50%_50%,var(--muted),transparent_40%)]">
+          <div className="absolute top-1/2 left-1/2 w-[16%] aspect-square bg-primary transform -translate-x-1/2 -translate-y-1/2 blur rounded-4xl"></div> 
+          <div className="">
+            <img src='/hero_banner.svg' alt = "logo" className="relative"/> 
           </div>
         </div>
-        <div className="relative px-20 py-3">
-          <h1 className="text-center text-6xl text-primary font-bold break-words">Seamless Volunteering</h1>
-          <p className="text-center text-1xl py-2 break-words">An open source platform connecting Science Olympiad tournaments to volunteers nationwide.</p>
+        
+        <div className="relative">
+            <h1 className="text-center xl:text-7xl text-4xl md:text-5xl text-primary font-bold break-words">Seamless Volunteering</h1>
+            <p className="text-center text-1xl py-2 break-words">An open source platform connecting Science Olympiad tournaments to volunteers nationwide.</p>
         </div>
         <div className="w-full flex gap-4 place-content-center-safe">
             <Link href="/signup">
-              <Button size="lg">Sign Up Today</Button>
+              <Button size="sm" className="text-bold">Sign Up Today</Button>
             </Link>
-          </div>
+        </div>
       </main>
     </>
   );
